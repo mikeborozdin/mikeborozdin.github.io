4b410507bff5dd9b1fb7a2899cb2556f74b03e00
Mike Borozdin <mikeborozdin@users.noreply.github.com>
2017-05-15 22:15:35 +0100

    Debugging build.sh

--- build.sh
+++ build.sh
@@ -24,6 +24,8 @@ echo "Cloning a git repo"
 #clone `master' branch of the repository using encrypted GH_TOKEN for authentification
 git clone https://${GH_TOKEN}@github.com/mikeborozdin/mikeborozdin.github.io.git ../mikeborozdin.github.io.master
 
+echo "# copy generated HTML site to `master' branch"
+
 # copy generated HTML site to `master' branch
 cp -R _site/* ../mikeborozdin.github.io.master
 
