0e1f4480298e1053330acf6125aadb368c6dab54
Mike Borozdin <mikeborozdin@users.noreply.github.com>
2017-05-15 22:25:48 +0100

    debug 1

--- build.sh
+++ build.sh
@@ -31,9 +31,9 @@ cp -R _site/* ../mikeborozdin.github.io.master
 
 # commit and push generated content to `master' branch
 # since repository was cloned in write mode with token auth - we can push there
-# cd ../mikeborozdin.github.io.master
-# git config user.email "mike.borozdin@gmail.com"
-# git config user.name "Mike Borozdin"
-# git add -A .
-# git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
+cd ../mikeborozdin.github.io.master
+git config user.email "mike.borozdin@gmail.com"
+git config user.name "Mike Borozdin"
+git add -A .
+git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
 # git push --quiet origin master > /dev/null 2>&1
