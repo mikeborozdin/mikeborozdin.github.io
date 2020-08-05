8fedba82239784c90a4994396ccdddaf27156ac1
Mike Borozdin <mike.borozdin@gmail.com>
2020-08-05 20:03:21 +0000

    Travis #75

--- js/toggle-menu.js
+++ js/toggle-menu.js
@@ -4,6 +4,6 @@ function toggleMenu() {
   if (menu.style.display === 'block') {
 	  menu.style.display = 'none';
   } else {
-		  menu.style.display = 'block';
+    menu.style.display = 'block';
   }
 }
